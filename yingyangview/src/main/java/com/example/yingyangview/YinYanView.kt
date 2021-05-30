85d321262d087c3f2a0396fa55fe2fccdf0368d6
Anwesh43 <anweshthecool0@gmail.com>
2021-05-30 07:17:37 +0530

    adding YinYanView class

--- yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
+++ yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawYYNode(i : Int, scale : Float, paint : Paint) {
     val h : Float = height.toFloat()
     drawYinYan(i, scale, w, h, paint)
 }
+
+class YinYanView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
