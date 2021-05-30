17a03b6ce2177ed46bd3208ff5b05f19a2a25a75
Anwesh43 <anweshthecool0@gmail.com>
2021-05-30 07:31:07 +0530

    addung YinYan class

--- yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
+++ yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
@@ -172,4 +172,27 @@ class YinYanView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class YinYan(var i : Int) {
+
+        private var curr : YYNode = YYNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *=-1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }
