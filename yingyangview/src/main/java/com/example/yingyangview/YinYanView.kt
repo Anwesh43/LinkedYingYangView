01e099ca16274d900f023a5c7a87ac90959039a1
Anwesh43 <anweshthecool0@gmail.com>
2021-05-30 06:56:28 +0530

    adding constants and libraries

--- yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
+++ yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
@@ -1,2 +1,26 @@
 package com.example.yingyangview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
