65c546d507ccc7a1807fa7ae4f446e6acfbc5539
Anwesh43 <anweshthecool0@gmail.com>
2021-05-30 07:29:28 +0530

    adding YYNode class

--- yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
+++ yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
@@ -131,4 +131,45 @@ class YinYanView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class YYNode(var i : Int, val state : State = State()) {
+
+        private var next : YYNode? = null
+        private var prev : YYNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = YYNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawYYNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : YYNode {
+            var curr : YYNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
 }
