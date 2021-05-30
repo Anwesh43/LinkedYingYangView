655575191a9e1cabe322d9d9d24a689aaa9dbb0c
Anwesh43 <anweshthecool0@gmail.com>
2021-05-30 06:57:59 +0530

    adding Int and Float extension methods

--- yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
+++ yingyangview/src/main/java/com/example/yingyangview/YinYanView.kt
@@ -24,3 +24,8 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
